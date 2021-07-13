1732a6a028aa24290d2143c2961ec3442150fdae
Jameel Evans <jameelevans@Jameels-iMac.local>
2021-07-13 12:36:44 -0600

    Finished V2 Enhancements

--- page-contact-us.php
+++ page-contact-us.php
@@ -12,22 +12,14 @@ get_header();
     <section class="general--single-narrow">
       <div class="contact-us">
         <div class="contact-us__content">
-          <p class="p__lead">Lorem ipsum dolor sit amet, consectetuer adipiscing
-        elit, sed diam nonummy nibh euismod tincidunt ut
-        laoreet dolore magna aliquam erat volutpat. Ut wisi
-        enim ad minim veniam, quis nostrud exerci tation
-        ullamcorper suscipit lobortis nisl ut aliquip ex ea
-        commodo consequat.</p>
+          <p class="p__lead">ICP TTA staff are available to answer any questions you might have about our program, to help you apply for no-cost assistance, or to find the resources you need to use for planning.</p>
         </div>
         
         <div class="contact">
             <h4 class="contact__header">Contact</h4>
-            <p class="contact__details">Lorem ipsum dolor sit amet,
-                consectetuer adipiscing elit,
-                sed diam nonummy nibh
-                euismod tincidunt ut laoreet</p>
-            <p><a href="" class="contact__link"><?php echo svg_icon('contact__icon', 'mail') ?>&nbsp;&nbsp;&nbsp; your@email.com</a></p>
-            <p><a href="" class="contact__link"><?php echo svg_icon('contact__icon', 'phone') ?>&nbsp; &nbsp;123-123-1234</a></p>
+            <p class="contact__details">To reach ICP TTA staff please email or call Tara Hughes at:</p>
+            <p><a href="mailto:tara.hughes@icf.com" class="contact__link"><?php echo svg_icon('contact__icon', 'mail') ?>&nbsp;&nbsp;&nbsp; tara.hughes@icf.com</a></p>
+            <p><a href="tel:6172183584" class="contact__link"><?php echo svg_icon('contact__icon', 'phone') ?>&nbsp; &nbsp;+1-617-218-3584</a></p>
         </div>
         
       </div>
