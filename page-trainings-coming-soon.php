584847aacb9dacbd356077fc1b532ce03762b88e
Jameel Evans <jameelevans@Jameels-iMac.local>
2021-07-21 11:28:50 -0600

    Finished V2 Enhancements

--- page-trainings-coming-soon.php
+++ page-trainings-coming-soon.php
@@ -16,7 +16,7 @@ get_header();
           <?php
             $today = date('Ymd');
             $trainings = new WP_Query(array(
-              'posts_per_page' => 3,
+              'posts_per_page' => -1,
               'post_type' => 'trainings',
               'meta_key' => 'training_date',
               'meta_value' => ' ',
