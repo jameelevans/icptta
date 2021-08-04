a6763d8644f16272a045a904d3f8b8c8a16b053f
Jameel Evans <jameelevans@Jameels-iMac.local>
2021-08-04 12:53:18 -0600

    Minor Enhancements

--- single-trainings.php
+++ single-trainings.php
@@ -17,7 +17,7 @@ get_header();
     <?php the_content(); ?>
     <h4 class="h4__header training__h4">To Request Training</h4>
     <p>To access these trainings or to request training not found here contact <strong>Terry Boes</strong> at terry.boes@icf.com or click the link below.</p>
-    <a href="mailto:terry.boes@icf.com?subject=Customized subject goes here" class="training__cta btn btn--blue" title="Click here to access training">Access Training</a>
+    <a href="mailto:terry.boes@icf.com?subject=Request training for <?php the_title(); ?>" class="training__cta btn btn--blue" title="Click here to access training">Access Training</a>
   </section>
 </main><!-- .Main content -->
 <?php get_footer(); ?>
