6d99bb9744531242402324e320b34f134440e837
Jameel Evans <jameelevans@Jameels-iMac.lan>
2021-10-19 11:05:43 -0600

    Updated Single training page language

--- single-trainings.php
+++ single-trainings.php
@@ -11,7 +11,7 @@ get_header();
 <!-- Main content -->
 <main class="main training general">
   <section class="training--narrow">
-    <p class="p__lead p__lead--centered">Recorded trainings are available for viewing by all who would benefit. After viewing, if you or your community are interested in additional information about a specific subject or would like assistance with customizing a webinar’s content to further your mass violence/domestic terrorism response planning, please contact the program at <a href="mailto:icptta@icf.com">icptta@icf.com</a>.</p>
+    <p class="p__lead p__lead--centered">The following trainings are available. After viewing, if you or your community are interested in additional information about a specific subject or would like assistance with customizing a webinar’s content to further your mass violence/domestic terrorism response planning, please contact the program at <a href="mailto:icptta@icf.com">icptta@icf.com</a>.</p>
   </section>
   <section class="training__content general--single-narrow">
     <?php the_content(); ?>
